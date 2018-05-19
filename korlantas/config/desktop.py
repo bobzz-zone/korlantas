a262f71d4a27cb2247da93e503e97120d3d935e2
bobzz-zone <bobzz.zone@gmail.com>
2018-05-19 14:20:35 +0700

    a

--- korlantas/config/desktop.py
+++ korlantas/config/desktop.py
@@ -5,10 +5,24 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Korlantas",
+			"module_name": "Master",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Korlantas")
+			"label": _("Master")
+		},
+		{
+			"module_name": "Harga",
+			"color": "grey",
+			"icon": "octicon octicon-repo",
+			"type": "module",
+			"label": _("Harga")
+		},
+		{
+			"module_name": "Stok",
+			"color": "grey",
+			"icon": "octicon octicon-package",
+			"type": "module",
+			"label": _("Stok")
 		}
 	]
